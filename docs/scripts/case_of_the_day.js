8518f9171e174ecbdb1b29f961192bfab880b5ac
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-31 16:26:00 +0300

    win

--- docs/scripts/case_of_the_day.js
+++ docs/scripts/case_of_the_day.js
@@ -49,6 +49,9 @@ imgElement.src = getRandomOption();*/
 
 
 // --------------------------------------------------------------
+
+
+
 let filteredOption
 let randomIndex
 const getRandomOption = () => {
@@ -61,30 +64,29 @@ const getRandomOption = () => {
     { name: "q6", img: "./images/cases/q6 case.png", link: "./CaseOpenPage/case_open_page_q6.html" },
   ];
 
-  const today = new Date();
-  const nextDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
-   filteredOption = nextDay.getDay() === 1 ? option.filter((_, i) => i !== 0).map(item => item.link) : option.map(item => item.link)
+  const today = new Date().toISOString().split('T')[0]; // получить сегодняшнюю дату в формате ГГГГ-ММ-ДД
+  const lastSelected = localStorage.getItem('lastSelected');
+  const lastSelectedDate = localStorage.getItem('lastSelectedDate');
 
-  // Проверяем, есть ли уже выбранный кейс в локальном хранилище
-  let selectedImagePath = localStorage.getItem("selectedImagePath");
-
-  if (!selectedImagePath || new Date(selectedImagePath) < nextDay) {
-    // Если нет или он устарел, выбираем новый кейс
-     randomIndex = Math.floor(Math.random() * filteredOption.length);
-    selectedImagePath = filteredOption[randomIndex].img;
-    localStorage.setItem("selectedImagePath", selectedImagePath);
-    console.log(filteredOption.length);
+  if (lastSelectedDate === today) {
+    // если сегодняшняя дата совпадает с датой последнего выбора, выбрать то же самое изображение
+    filteredOption = option.filter(opt => opt.name === lastSelected);
+  } else {
+    // иначе, выбрать новое изображение
+    const randomIndex = Math.floor(Math.random() * option.length);
+    filteredOption = [option[randomIndex]];
+    localStorage.setItem('lastSelected', filteredOption[0].name);
+    localStorage.setItem('lastSelectedDate', today);
   }
-  return selectedImagePath;
+
+  return filteredOption;
 };
+const selectedOption = getRandomOption();
+const imgSrc = document.querySelector(".day_case");
+const linkHref = document.querySelector(".day")
 
-const imgElement = document.querySelector(".day_case");
-imgElement.src = getRandomOption();
-
-let aElement = document.querySelector(".day");
-
-
-
+imgSrc.src = selectedOption[0].img;
+linkHref.href = selectedOption[0].link;
 
 
 
